eb9caf55958dc8eccae41dd4d34ec1500791da25
TakutoIyanagi-littletree <131866009+TakutoIyanagi-littletree@users.noreply.github.com>
2023-11-24 15:55:27 +0900

    Create log_update.sh

---
+++ log_update.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# リポジトリへのパス。これを適切なものに変更してください。
+REPO_PATH="/path/to/your/repository"
+
+# 更新したいファイルのパス
+FILE_PATH="$REPO_PATH/yourfile.txt"
+
+# Gitリポジトリに移動
+cd $REPO_PATH
+
+# ファイルを更新（ここでは例としてファイルに日付と時刻を追加）
+echo "Updated on $(date)" >> $FILE_PATH
+
+# 変更をステージング
+git add $FILE_PATH
+
+# コミットを作成
+git commit -m "Updated the file with the current date and time"
+
+# リモートリポジトリにプッシュ
+git push origin master
+
+echo "File updated and pushed to GitHub"
